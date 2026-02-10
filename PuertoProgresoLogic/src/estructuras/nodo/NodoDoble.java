4d964e9224a03195cea64e8250de6004a7d26341
Juan Canche <a24216370@alumnos.uady.mx>
2026-02-09 22:30:14 -0600

    modificacion archivos nodos

--- PuertoProgresoLogic/src/estructuras/nodo/NodoDoble.java
+++ PuertoProgresoLogic/src/estructuras/nodo/NodoDoble.java
@@ -1,5 +1,13 @@
 package estructuras.nodo;
 
 public class NodoDoble {
+    public Object dato;
+    public NodoDoble siguiente;
+    public NodoDoble anterior;
 
+    public NodoDoble(Object dato) {
+        this.dato = dato;
+        this.siguiente = null;
+        this.anterior = null;
+    }
 }
