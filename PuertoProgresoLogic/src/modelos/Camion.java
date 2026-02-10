94f72327f9fd579caea14dcb38dcc150ee9a5034
Juan Canche <a24216370@alumnos.uady.mx>
2026-02-09 22:42:52 -0600

    actulizacion del archivo camion

--- PuertoProgresoLogic/src/modelos/Camion.java
+++ PuertoProgresoLogic/src/modelos/Camion.java
@@ -1,5 +1,32 @@
 package modelos;
 
 public class Camion {
+    private String placa;
+    private String empresa;
 
+    public Camion(String placa, String empresa) {
+        this.placa = placa;
+        this.empresa = empresa;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    @Override
+    public String toString() {
+        return "Camion [Placa=" + placa + ", Empresa=" + empresa + "]";
+    }
 }
