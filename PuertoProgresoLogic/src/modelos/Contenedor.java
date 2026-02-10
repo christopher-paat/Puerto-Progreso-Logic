0314505e8fe8d5c045372d6fa0c7f144160682fb
Juan Canche <a24216370@alumnos.uady.mx>
2026-02-09 22:47:13 -0600

    actualizacion del archivo contenedor

--- PuertoProgresoLogic/src/modelos/Contenedor.java
+++ PuertoProgresoLogic/src/modelos/Contenedor.java
@@ -1,5 +1,26 @@
 package modelos;
 
-public class Contenedor {
+import estructuras.lista.ListaSimple;
 
+public class Contenedor {
+    private String codigo;
+    private ListaSimple productos;
+
+    public Contenedor(String codigo) {
+        this.codigo = codigo;
+        this.productos = new ListaSimple();
+    }
+
+    public void agregarProducto(Producto p) {
+        productos.insertar(p);
+    }
+
+    public double calcularPesoTotal() {
+        return productos.calcularPeso();
+    }
+
+    @Override
+    public String toString() {
+        return "Contenedor [" + codigo + "]";
+    }
 }
