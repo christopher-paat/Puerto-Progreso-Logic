0c4d78b3cff6f1cbb140af568ddae6b1b901c6df
Juan Canche <a24216370@alumnos.uady.mx>
2026-02-09 22:44:08 -0600

    actualizacion del archivo producto

--- PuertoProgresoLogic/src/modelos/Producto.java
+++ PuertoProgresoLogic/src/modelos/Producto.java
@@ -1,5 +1,42 @@
 package modelos;
 
 public class Producto {
+    private int id;
+    private String nombre;
+    private double peso;
 
+    public Producto(int id, String nombre, double peso) {
+        this.id = id;
+        this.nombre = nombre;
+        this.peso = peso;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    @Override
+    public String toString() {
+        return id + " - " + nombre + " (" + peso + " kg)";
+    }
 }
