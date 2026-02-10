0403a6cb1dc64e2af3cd4dd8108969f0551de825
Juan Canche <a24216370@alumnos.uady.mx>
2026-02-09 22:41:01 -0600

    actuzalicion de los archivos listas simple/doble

--- PuertoProgresoLogic/src/estructuras/lista/ListaSimple.java
+++ PuertoProgresoLogic/src/estructuras/lista/ListaSimple.java
@@ -1,5 +1,34 @@
 package estructuras.lista;
 
-public class ListaSimple {
+import estructuras.nodo.NodoSimple;
 
+public class ListaSimple {
+    private NodoSimple cabeza;
+
+    public ListaSimple() {
+        cabeza = null;
+    }
+
+    public void insertar(Object dato) {
+        NodoSimple nuevo = new NodoSimple(dato);
+        nuevo.siguiente = cabeza;
+        cabeza = nuevo;
+    }
+
+    public boolean buscar(Object dato) {
+        NodoSimple aux = cabeza;
+        while (aux != null) {
+            if (aux.dato.equals(dato)) return true;
+            aux = aux.siguiente;
+        }
+        return false;
+    }
+
+    public void recorrer() {
+        NodoSimple aux = cabeza;
+        while (aux != null) {
+            System.out.println(aux.dato);
+            aux = aux.siguiente;
+        }
+    }
 }
