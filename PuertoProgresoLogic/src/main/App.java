c588b4180b85d17229227131ada286e0f286063a
Juan Canche <a24216370@alumnos.uady.mx>
2026-02-14 19:59:14 -0600

    actualizacion del archivo app

--- PuertoProgresoLogic/src/main/App.java
+++ PuertoProgresoLogic/src/main/App.java
@@ -1,6 +1,31 @@
 package main;
+
+import modulos.ModuloPatio;
+import modulos.ModuloRecepcion;
+import modulos.ModuloRutas;
+import utils.Entrada;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ModuloRecepcion recepcion = new ModuloRecepcion();
+        ModuloPatio patio = new ModuloPatio();
+        ModuloRutas rutas = new ModuloRutas();
+
+        int opcion;
+        do {
+            System.out.println("\nPUERTO PROGRESO LOGIC");
+            System.out.println("[1] Recepción");
+            System.out.println("[2] Patio");
+            System.out.println("[3] Rutas");
+            System.out.println("[4] Salir");
+
+            opcion = Entrada.leerInt();
+
+            switch (opcion) {
+                case 1 -> recepcion.menu();
+                case 2 -> patio.menu();
+                case 3 -> rutas.menu();
+            }
+        } while (opcion != 4);
     }
 }
